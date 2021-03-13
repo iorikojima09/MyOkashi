1e48ab336fb9af27cbd45c7bc930045403bf2e91
Iori Kojima <kojima.iori@gmail.com>
2021-03-13 11:48:17 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 小島伊織 on 2021/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
